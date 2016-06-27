483250d21b683f9a1a5cf904b6386a08a6f65535
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-28 00:42:08 +0530

    Layout for getting request created

--- app/src/main/java/com/example/gohan/volson/MainActivity.java
+++ app/src/main/java/com/example/gohan/volson/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.gohan.volson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView name,email,mob;
+    Button button;
+    String server_url = "http://192.168.26.7:8080/Learn/volSon/info.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = (TextView) findViewById(R.id.textView);
+        email= (TextView) findViewById(R.id.textView2);
+        mob = (TextView) findViewById(R.id.textView3);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
